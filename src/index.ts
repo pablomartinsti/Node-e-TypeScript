b0383593bd69855c3db9d89a81891ca78e09aa5c
Pablo Martins <pablomartinsti@outlook.com>
2026-07-03 00:08:28 -0300

    feat: instalação do express e criaçao das primeiras rotas GET,POST

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Node com TypeScript funcionando!');
+import express, { Request, Response } from 'express';
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'DioBank API' });
+});
+
+server.post('/user', (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+  return res.status(201).json({ message: 'Usuário criado' });
+});
+
+server.listen(5000, () => console.log('Server on'));
